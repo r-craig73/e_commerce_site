75b803a62e8db913177526f11a4056e57c59c752
Guest User <Guest@Epicodus-4C.local>
2018-05-01 10:23:37 -0700

    edit files to pass two API calls, using one method

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,15 +1,7 @@
 class Book
 
-  def self.call_is_ok
-      api_check = RestClient::Request.execute(method: :get, url: 'https://api.nytimes.com/svc/books/v3/lists/combined-print-and-e-book-fiction.json', headers: {api_key: ENV['NYT_API_KEY']})
-      JSON.parse(api_check)["status"]
-  end
-
-
   def self.get_bestseller_list
     response = RestClient::Request.execute(method: :get, url: 'https://api.nytimes.com/svc/books/v3/lists/combined-print-and-e-book-fiction.json', headers: {api_key: ENV['NYT_API_KEY']})
-    # JSON.parse(response)["results"]["books"][0]["title"]  # returns the first title
-    # JSON.parse(response)["results"]
   end
 
 end
